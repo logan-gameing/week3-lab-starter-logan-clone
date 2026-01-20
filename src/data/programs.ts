c0fca3ef2d5b1ac6c9c5d175b52166804a7004ea
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-01-20 11:16:42 -0700

    Worked on Data and Types
    Completed the todo in Data and Types

--- src/data/programs.ts
+++ src/data/programs.ts
@@ -1,5 +1,44 @@
 import type { Program } from "../types/program";
 
 export const programs: Program[] = [
-  // add programs here
+  {
+    id: "300",
+    name: "Data Development",
+    credential: "Diploma",
+    school: "School for Advanced Digital Technology",
+    years: 1,
+    credits: 2,
+  },
+  {
+    id: "301",
+    name: "Data Development 2",
+    credential: "Diploma",
+    school: "School for Advanced Digital Technology",
+    years: 1,
+    credits: 2,
+  },
+  {
+    id: "302",
+    name: "Java Programming 1-3",
+    credential: "Diploma",
+    school: "School for Advanced Digital Technology",
+    years: 3,
+    credits: 5,
+  },
+  {
+    id: "303",
+    name: "Energy Use in the Workplace",
+    credential: "Certificate",
+    school: "MacPhail School of Energy",
+    years: 3,
+    credits: 5,
+  },
+  {
+    id: "304",
+    name: "Touring 1",
+    credential: "Degree",
+    school: "School of Hospitality and Tourism",
+    years: 1,
+    credits: 3,
+  },
 ];
--- src/types/program.ts
+++ src/types/program.ts
@@ -1,6 +1,10 @@
+import { Delivery, School } from "./common.js";
 // TODO: Create a type for program ids, which should be strings
+export type ProgramId = "300" | "301" | "302" | "303" | "304";
 
 // TODO: Create a type for credential types, which can either be "Diploma", "Degree", or "Certificate"
+
+export type Credential = "Diploma" | "Degree" | "Certificate";
 
 // TODO: Create an interface for programs
 // The interface should include the following properties:
@@ -14,4 +18,14 @@
 // an optional array of career paths
 // an optional note
 
-export interface Program {}
+export interface Program {
+  id: ProgramId;
+  name: string;
+  credential: Credential;
+  school: School;
+  years: number;
+  credits: number;
+  delivery?: Delivery;
+  careerpath?: Array<1>;
+  note?: string;
+}
