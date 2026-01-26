e2bc3e284970e2efa60a28eb3f65b24b062e75e8
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-01-26 09:56:20 -0700

    Minor commit

--- src/ui/programs/ProgramList.tsx
+++ src/ui/programs/ProgramList.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import ProgramCard from "./ProgramCard";
 
 export default function ProgramList() {
   // Optional TODO: Sort programs by name before rendering by copying the programs array and sorting the copy
@@ -7,7 +8,11 @@ export default function ProgramList() {
   // The root component should be a View for the list, and inside of the view we should map over the programs array
   // For each program, render a ProgramCard component, passing the program as a prop
   // Remember to set a key prop on each ProgramCard, using the program id (this allows React to track each component efficiently)
-  return null;
+  return (
+    <View style={styles.list}>
+      <ProgramCard>{"303"}</ProgramCard>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
