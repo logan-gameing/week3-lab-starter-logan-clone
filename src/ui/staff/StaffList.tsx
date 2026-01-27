a6830c62ad9c16513925e874ac864cf6a99787c9
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-01-27 11:23:10 -0700

    Finished StaffList

--- src/ui/staff/StaffList.tsx
+++ src/ui/staff/StaffList.tsx
@@ -1,5 +1,6 @@
 import { staff } from "@/src/data/staff";
 import { StyleSheet, View } from "react-native";
+import StaffCard from "./StaffCard";
 
 export default function StaffList() {
   // Optional TODO: Sort staff by name before rendering by copying the staff array and sorting the copy
@@ -13,7 +14,13 @@ export default function StaffList() {
   // The root component should be a View for the list, and inside of the view we should map over the staff array
   // For each person, render a StaffCard component, passing the person as a prop
   // Remember to set a key prop on each StaffCard, using the person's id (this allows React to track each component efficiently)
-  return <View style={styles.list}></View>;
+  return (
+    <View style={styles.list}>
+      {stafflist.map((res) => (
+        <StaffCard staff={res} key={res.id} />
+      ))}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
