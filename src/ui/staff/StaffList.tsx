04e7828ea8fd61173a54e7ac1e1b9d0427d3be3c
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-01-27 11:21:34 -0700

    Update Stafflist

--- src/ui/staff/StaffList.tsx
+++ src/ui/staff/StaffList.tsx
@@ -1,10 +1,11 @@
 import { staff } from "@/src/data/staff";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function StaffList() {
   // Optional TODO: Sort staff by name before rendering by copying the staff array and sorting the copy
 
-  let stafflist: stafflist = staff;
+  let stafflist;
+  stafflist = staff;
 
   stafflist.sort((a, b) => a.name.localeCompare(b.name));
 
@@ -12,7 +13,7 @@ export default function StaffList() {
   // The root component should be a View for the list, and inside of the view we should map over the staff array
   // For each person, render a StaffCard component, passing the person as a prop
   // Remember to set a key prop on each StaffCard, using the person's id (this allows React to track each component efficiently)
-  return null;
+  return <View style={styles.list}></View>;
 }
 
 const styles = StyleSheet.create({
