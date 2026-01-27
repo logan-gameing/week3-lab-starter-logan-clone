164ee8616e245b46ddf41d7f69955ebe20d5da26
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-01-27 11:30:24 -0700

    Finished StaffSection (wip)
    Got it "finished" but it doesnt work for some reason.

--- src/ui/staff/StaffSection.tsx
+++ src/ui/staff/StaffSection.tsx
@@ -1,6 +1,15 @@
+import Section from "../components/Section";
+import ProgramList from "../programs/ProgramList";
 
 export default function StaffSection() {
   // TODO: Write the tsx code to render a Section component
   // Inside the Section, render the StaffList component
-  return null;
+  return (
+    <Section
+      title="Campus Staff"
+      subtitle="Learn about our beloved campus staff"
+    >
+      <ProgramList />
+    </Section>
+  );
 }
